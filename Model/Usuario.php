dec0641cd6bdda150bd3e59b307824eda0a14f13
cilop <cilop@cilop-SVF14211CLB>
2015-05-30 20:05:59 -0600

    Clase Usuario

---
+++ Model/Usuario.php
@@ -0,0 +1,47 @@
+<?php
+
+class Usuario {
+    private $_userName;
+    private $_pass;
+    private $_tipo;
+    private $_correo;
+    
+    public function __construct() {
+        
+    }
+    
+    function getUserName() {
+        return $this->_userName;
+    }
+
+    function getPass() {
+        return $this->_pass;
+    }
+
+    function getTipo() {
+        return $this->_tipo;
+    }
+
+    function getCorreo() {
+        return $this->_correo;
+    }
+
+    function setUserName($_userName) {
+        $this->_userName = $_userName;
+    }
+
+    function setPass($_pass) {
+        $this->_pass = $_pass;
+    }
+
+    function setTipo($_tipo) {
+        $this->_tipo = $_tipo;
+    }
+
+    function setCorreo($_correo) {
+        $this->_correo = $_correo;
+    }
+
+
+    
+}
